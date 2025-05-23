4e8cf228fd64e0ac2296f40628d67861d420388a
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-05-23 16:21:04 -0400

    fleet expanded

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -140,18 +140,28 @@ class SpaceInvaders:
         # Add aliens until there is no room left.
         # Space between aliens is one alien in width
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size # Tuple containing width and height
         
-        # we get the based width from the first alien
-        current_x = alien_width
-        # takes the screen width and subtracts 2 times the alien width
-        # this calculates the maximum number of aliens that can be spawn in with a space of 1 entire alien
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
+        # we get the base dimensions
+        current_x, current_y = alien_width, alien_height
+
+        # Spawn logic
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            # Finished a row, reset x value and increment y value
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """Method to spawn aliens."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _spawn_boss(self):
         """A manager for the boss classes."""
