ec1b6931b235e71702cb071905e088a7d7281818
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-05-25 14:40:16 -0400

    adding constants file

---
+++ space_invaders/constants.py
@@ -0,0 +1,9 @@
+"""Class to manage constant values to declutter and simplify the other modules."""
+
+class SIConstants:
+
+    def __init__(self):
+        """Initialize constant values."""
+        self.SCREEN_WIDTH = 1200
+        self.SCREEN_HEIGHT = 800
+        self.DARK_PURPLE = (40, 0, 80) # Background color
